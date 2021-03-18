3fda3ccf380136f969710decd7de884f40281150
martinjrz <teksojr98@gmail.com>
2021-03-18 18:05:03 +0545

    cancel fetch request

--- react-on-host/src/product.js
+++ react-on-host/src/product.js
@@ -9,26 +9,10 @@ export default function Product() {
     return (
         <div>
             <h1>This is the product page </h1>
-            <form onSubmit={(e)=>{
-                e.preventDefault()
-              const  data={
-                    name:name
-                }
-                fetch({method:"post",url:"/postuser",
-            body:JSON.stringify(data)
-            })
-            }}>
+            <form>
                 <input onChange={(e)=>setter(e)} value={name} placeholder='enter name'>
                 </input>
-                <button onClick={(e)=>{
-                    e.preventDefault()
-              const  data={
-                    name:name
-                }
-                fetch({method:"post",url:"http://test-react-deploy-app.herokuapp.com/",
-            body:JSON.stringify(data)
-            })
-            }}>send</button>
+                <button >send</button>
             </form>
         </div>
     )
